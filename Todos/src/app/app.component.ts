b949c15835fca5d4345f299f241a21cdfe27ec25
LuanMarcosCosta <luanmarcoscosta@outlook.com>
2020-04-10 09:51:18 -0300

    Aula 17 Balta.Io

--- Todos/src/app/app.component.ts
+++ Todos/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todos.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Todos';
+  public todos: Todo[] = [];
+  public title: String = 'Minhas Tarefas';
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required,
+      ])]
+    })
+    this.load();
+  }
+
+  clear() {
+    this.form.reset();
+  }
+
+  add() {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index != -1) {
+      this.todos.splice(index, 1);
+
+    }
+    this.save(); //após executar a verificação, você consegue salvar
+  }
+  // 
+  markAsDone(todo: Todo) {
+    todo.done = true;
+    this.save();
+
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+    this.save();
+
+  }
+
+  // Salvando o código dos Todos temporariamente
+  save() {
+    const data = JSON.stringify(this.todos); //convertendo um JASON em uma string
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    if (data) {
+      this.todos = JSON.parse(data); //String em um JSON.
+    } else {
+      this.todos = [];
+    }
+  }
+
 }
